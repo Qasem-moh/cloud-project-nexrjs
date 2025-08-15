b218ecdd87e2a238c4c3cc23b4455e4ca188562c
Qasem-moh <qasemcoder2020@gmail.com>
2025-08-15 04:25:48 +0300

    next

--- next-cloud/src/components/Header/Header.tsx
+++ next-cloud/src/components/Header/Header.tsx
@@ -1,26 +1,12 @@
 import Link from 'next/link'
-import { GrTechnology } from "react-icons/gr";
 import styles from "./Header.module.css"
+import NavBar from './NavBar';
 
 const Header = () => {
   return (
     <div>
       <header className={styles.header}>
-        <nav className={styles.navbar}>
-          <div>
-            <Link href="/" className={styles.logo}>
-              CLOUD
-              <GrTechnology />
-              HOSTING
-            </Link>
-          </div>
-          <ul className={styles.navLinks}>
-            <Link className={styles.navLink} href="/">Home</Link>
-            <Link className={styles.navLink} href="/articles">Articles</Link>
-            <Link className={styles.navLink}  href="/about">About</Link>
-            <Link className={styles.navLink}  href="/admin">Admin</Link>
-          </ul>
-        </nav>
+       <NavBar />
         <div className={styles.right}>
           <Link className={styles.btn} href="/login">Login</Link>
           <Link className={styles.btn} href="/register">Register</Link>
---
+++ next-cloud/src/components/Header/NavBar.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import { GrTechnology } from "react-icons/gr";
+import styles from "./Header.module.css"
+
+const NavBar = () => {
+  return (
+     <nav className={styles.navbar}>
+          <div>
+            <Link href="/" className={styles.logo}>
+              CLOUD
+              <GrTechnology />
+              HOSTING
+            </Link>
+          </div>
+          <ul className={styles.navLinks}>
+            <Link className={styles.navLink} href="/">Home</Link>
+            <Link className={styles.navLink} href="/articles">Articles</Link>
+            <Link className={styles.navLink}  href="/about">About</Link>
+            <Link className={styles.navLink}  href="/admin">Admin</Link>
+          </ul>
+        </nav>
+  )
+}
+
+export default NavBar
