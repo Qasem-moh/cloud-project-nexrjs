d4ee6f01e98c5cf2d6b1505b159b583ae40c0b6d
Qasem-moh <qasemcoder2020@gmail.com>
2025-08-15 05:28:04 +0300

    next

--- next-cloud/src/components/Header/Header.tsx
+++ next-cloud/src/components/Header/Header.tsx
@@ -1,19 +1,23 @@
-import Link from 'next/link'
-import styles from "./Header.module.css"
-import NavBar from './NavBar';
+import Link from "next/link";
+import styles from "./Header.module.css";
+import NavBar from "./NavBar";
 
 const Header = () => {
   return (
     <div>
       <header className={styles.header}>
-       <NavBar />
+        <NavBar />
         <div className={styles.right}>
-          <Link className={styles.btn} href="/login">Login</Link>
-          <Link className={styles.btn} href="/register">Register</Link>
+          <Link className={styles.btn} href="/login">
+            Login
+          </Link>
+          <Link className={styles.btn} href="/register">
+            Register
+          </Link>
         </div>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- next-cloud/src/components/Header/NavBar.tsx
+++ next-cloud/src/components/Header/NavBar.tsx
@@ -1,25 +1,55 @@
-import Link from 'next/link'
+"use client";
+import Link from "next/link";
 import { GrTechnology } from "react-icons/gr";
-import styles from "./Header.module.css"
-
+import styles from "./Header.module.css";
+import { CiMenuBurger } from "react-icons/ci";
+import { useState } from "react";
+import { IoCloseCircle } from "react-icons/io5";
 const NavBar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
-     <nav className={styles.navbar}>
-          <div>
-            <Link href="/" className={styles.logo}>
-              CLOUD
-              <GrTechnology />
-              HOSTING
-            </Link>
-          </div>
-          <ul className={styles.navLinks}>
-            <Link className={styles.navLink} href="/">Home</Link>
-            <Link className={styles.navLink} href="/articles">Articles</Link>
-            <Link className={styles.navLink}  href="/about">About</Link>
-            <Link className={styles.navLink}  href="/admin">Admin</Link>
-          </ul>
-        </nav>
-  )
-}
+    <nav className={styles.navbar}>
+      <div>
+        <Link href="/" className={styles.logo}>
+          CLOUD
+          <GrTechnology />
+          HOSTING
+        </Link>
+        {toggle ? (
+          <IoCloseCircle
+            className={styles.closeIcon}
+            onClick={() => setToggle(!toggle)}
+          />
+        ) : (
+          <CiMenuBurger
+            className={styles.menuIcon}
+            onClick={() => setToggle(!toggle)}
+          />
+        )}
+      </div>
+      <div
+        className={styles.navLinksWrapper}
+        style={{
+          clipPath: (toggle && "polygon(0 0, 100% 0, 100% 100%, 0 100%)") || "",
+        }}
+      >
+        <ul className={styles.navLinks}>
+          <Link className={styles.navLink} href="/">
+            Home
+          </Link>
+          <Link className={styles.navLink} href="/articles">
+            Articles
+          </Link>
+          <Link className={styles.navLink} href="/about">
+            About
+          </Link>
+          <Link className={styles.navLink} href="/admin">
+            Admin
+          </Link>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
